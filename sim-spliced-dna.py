ca9ffb39338e9d4d8d096189f371e8b714fa23c5
William Majoros <bmajoros@dcc-login-01.oit.duke.edu>
2021-10-14 15:45:33 -0400

    update

--- sim-spliced-dna.py
+++ sim-spliced-dna.py
@@ -11,15 +11,19 @@ from builtins import (bytes, dict, int, list, object, range, str, ascii,
 import sys
 import ProgramName
 import random
+import copy
 from GffTranscriptReader import GffTranscriptReader
 from Pipe import Pipe
 from ConfigFile import ConfigFile
 from Translation import Translation
 
+# DEPENDENCIES:
+#   module load htslib
+
 class Variant:
     def __init__(self,fields):
         self.Chr=fields[0]
-        self.pos=int(fields[1])
+        self.genomicPos=int(fields[1])
         self.ref=fields[3]
         self.alt=fields[4]
 
@@ -30,6 +34,20 @@ class SamRecord:
         self.readLen=len(fields[9])
         self.qual=fields[10]
 
+def makeAltGene(gene,variants):
+    altGene=copy.deepcopy(gene)
+    changes=False
+    for transcript in gene.transcripts:
+        array=list(transcript.sequence)
+        for variant in variants:
+            pos=transcript.mapToTranscript(variant.genomicPos)
+            #print("strlen=",len(transcript.sequence)," genomicPos=",variant.genomicPos,"transcriptPos=",pos)
+            if(pos<0): continue
+            array[pos]=variant.alt
+            changes=True
+        transcript.sequence="".join(array)
+    return altGene if changes else None
+        
 def nextSamRec(fh,filename):
     while(True):
         line=fh.readline()
@@ -44,60 +62,73 @@ def nextSamRec(fh,filename):
         break
     return SamRecord(fields)
 
-def loadVCF(filename):
+def tabixVCF(vcfFile,Chr,begin,end):
     records=[]
-    with open(filename,"rt") as IN:
-        for line in IN:
-            if(len(line)==0 or line[0]=="#"): continue
-            fields=line.rstrip().split()
-            variant=Variant(fields)
-            records.append(variant)
+    cmd="tabix "+vcfFile+" "+Chr+":"+str(begin)+"-"+str(end)
+    lines=Pipe.run(cmd)
+    for line in lines.split("\n"):
+        if(len(line)==0 or line[0]=="#"): continue
+        fields=line.rstrip().split()
+        variant=Variant(fields)
+        records.append(variant)
     return records
 
-#def simRead(L,genome,path,Chr,start,end):
-#    cmd=path+"/twoBitToFa -seq="+Chr+" -start="+str(start)+\
-#        " -end="+str(end)+" "+genome+" stdout"
-#    output=Pipe.run(cmd)
-#    fields=output.rstrip().split("\n")
-#    seq=fields[1] ### ?
-#    return seq
-
-def simRead(transcript,rec1,rec2,genome,path):
-    L=len(transcript.sequence)
+def simRead(refTranscript,altTranscript,rec1,rec2,genome,path):
+    L=len(refTranscript.sequence)
     if(L<rec1.fragLen or L<rec1.readLen or L<rec2.readLen): return None
     lastStart=L-rec1.fragLen
-    #print("L=",L,"fraglen=",rec1.fragLen)
     start1=random.randrange(lastStart+1)
     end1=start1+rec1.readLen
     end2=start1+rec1.fragLen
     start2=end2-rec2.readLen
-    seq1=readFrom2bit(genome,path,transcript.getSubstrate(),start1,end1)
-    seq2=readFrom2bit(genome,path,transcript.getSubstrate(),start2,end2)
-    return (seq1,rec1.qual,seq2,rec2.qual)
+    refSeq1=refTranscript.sequence[start1:end1]
+    refSeq2=refTranscript.sequence[start2:end2]
+    altSeq1=altTranscript.sequence[start1:end1]
+    altSeq2=altTranscript.sequence[start2:end2]
+    return (refSeq1,altSeq1,rec1.qual,refSeq2,altSeq2,rec2.qual)
     
 def readFrom2bit(genome,path,Chr,start,end):
     cmd=path+"/twoBitToFa -seq="+Chr+" -start="+str(start)+\
         " -end="+str(end)+" "+genome+" stdout"
     output=Pipe.run(cmd)
     fields=output.rstrip().split("\n")
-    seq=fields[1]
+    seq=""
+    for i in range(1,len(fields)): seq+=fields[i]
     return seq
 
 def loadTranscriptSeqs(gene,genome,path):
     Chr=gene.getSubstrate()
+    strand=gene.getStrand()
     for transcript in gene.transcripts:
+        #print("gene=",gene.getID()," transcript=",transcript.getID())
         transcript.sequence=""
-        transcript.getRawExons()
+        transcript.exons=transcript.getRawExons()
+        #print(len(transcript.exons),"exons")
         for exon in transcript.rawExons:
             exon.sequence=readFrom2bit(genome,path,Chr,exon.begin,exon.end)
+            #print("exon="+str(exon.begin)+"-"+str(exon.end),"seq=",len(exon.sequence))
+            exon.sequence=exon.sequence.upper()
+            if(strand=="-"):
+                exon.sequence=Translation.reverseComplement(exon.sequence)
+            if("N" in exon.sequence):
+                raise Exception("N FOUND: Chr="+Chr+"begin="+str(exon.begin)+\
+                                "end="+str(exon.end))
             transcript.sequence+=exon.sequence
-    if(gene.getStrand()=="-"):
-        transcript.sequence=Translation.reverseComplement(transcript.sequence)
+            #print("TRANSCRIPT=",transcript.sequence,"added",len(exon.sequence),"exon="+str(exon.begin)+"-"+str(exon.end))
+#    if(gene.getStrand()=="-"):
         
-def pickTranscript(gene):
-    n=gene.getNumTranscripts()
+def pickTranscript(refGene,altGene):
+    n=refGene.getNumTranscripts()
     i=random.randrange(n)
-    return gene.getIthTranscript(i)
+    return (refGene.getIthTranscript(i),altGene.getIthTranscript(i))
+
+def getGeneVariants(gene,vcfFile):
+    Chr=gene.getSubstrate()
+    return tabixVCF(vcfFile,gene.getSubstrate(),gene.getBegin(),
+                    gene.getEnd())
+
+def printRead(header,seq,qual):
+    print(header+"\n"+seq+"\n+\n"+qual)
 
 #=========================================================================
 # main()
@@ -116,8 +147,8 @@ samFile=configFile.lookupOrDie("aligned-rna")
 gffFile=configFile.lookupOrDie("gff")
 
 # Load VCF
-print("reading VCF...",file=sys.stderr,flush=True)
-loadVCF(vcfFile)
+#print("reading VCF...",file=sys.stderr,flush=True)
+#loadVCF(vcfFile)
 
 # Load GFF
 gffReader=GffTranscriptReader()
@@ -130,25 +161,25 @@ nextReadID=1
 IN=open(samFile,"rt")
 for gene in genes:
     loadTranscriptSeqs(gene,genome2bit,twoBitDir)
+    variants=getGeneVariants(gene,vcfFile)
+    if(len(variants)==0): continue
+    refGene=gene
+    altGene=makeAltGene(gene,variants)
+    if(altGene is None): continue # no variants in exons
+    #print(len(variants),"variants in this gene")
     for i in range(NUM_READS):
-        transcript=pickTranscript(gene)
+        (refTranscript,altTranscript)=pickTranscript(refGene,altGene)
         rec1=nextSamRec(IN,samFile)
         rec2=nextSamRec(IN,samFile)
-        Chr="chr1" ### DEBUGGING
-        start1=100000; end1=100100; start2=200000; end2=200100; ### DEBUGGING
-        #seq1=simRead(rec1.readLen,genome2bit,twoBitDir,Chr,start1,end1)
-        #seq2=simRead(rec2.readLen,genome2bit,twoBitDir,Chr,start2,end2)
-        sim=simRead(transcript,rec1,rec2,genome2bit,twoBitDir)
+        sim=simRead(refTranscript,altTranscript,rec1,rec2,genome2bit,
+                    twoBitDir)
         if(sim is None): break # gene is shorter than fragment length
-        (seq1,qual1,seq2,qual2)=sim
-        print("@READ"+str(nextReadID),"SIM")
-        print(seq1)
-        print("+")
-        print(qual1)
-        print("@READ"+str(nextReadID),"SIM")
-        print(seq2)
-        print("+")
-        print(qual2)
+        (refSeq1,altSeq1,qual1,refSeq2,altSeq2,qual2)=sim
+        printRead("@READ"+str(nextReadID)+" SIM",refSeq1,qual1)
+        printRead("@READ"+str(nextReadID)+" SIM",refSeq2,qual2)
+        nextReadID+=1
+        printRead("@READ"+str(nextReadID)+" SIM",altSeq1,qual1)
+        printRead("@READ"+str(nextReadID)+" SIM",altSeq2,qual2)
         nextReadID+=1
 
 
